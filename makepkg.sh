52772674f1dd9ceab364d090793eb950fb231f25
Mark Striekwold <strmark@users.noreply.github.com>
2020-10-07 20:26:51 +0200

    Uploaded files

---
+++ makepkg.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# This script is for Manjaro Linux to download and install XRDP+XORGXRDP
+#
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo 'This script must be run as a non-root user, as building packages as root is unsupported.' >&2
+    exit 1
+fi
+
+###############################################################################
+# Prepare by installing build tools.
+#
+# Partial upgrades aren't supported in arch.
+sudo pacman -Syu --needed --noconfirm base base-devel git
+
+# Create a build directory in tmpfs
+TMPDIR=$(mktemp -d)
+pushd "$TMPDIR" || exit
+
+###############################################################################
+# XRDP
+#
+(
+	git clone https://aur.archlinux.org/xrdp-git.git
+	cd xrdp-git || exit
+	makepkg -sri --noconfirm
+)
+###############################################################################
+# XORGXRDP
+# Devel version, because release version includes a bug crashing gnome-settings-daemon
+(
+	git clone https://aur.archlinux.org/xorgxrdp-git.git
+	cd xorgxrdp-git || exit
+	makepkg -sri --noconfirm
+)
+###############################################################################
+
+#remove build directory
+rm -rf $TMPDIR
